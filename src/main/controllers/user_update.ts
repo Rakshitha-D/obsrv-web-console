bdabc83495df31155d3b61638bd6559f41dc31a4
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-08-20 15:16:55 +0530

    #OBS-I169: modified the update api

--- src/main/controllers/user_update.ts
+++ src/main/controllers/user_update.ts
@@ -1,16 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
 import userService from '../services/oauthUsers';
+import bcrypt from 'bcryptjs';
 
 export default {
     name: 'user:update',
     handler: () => async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { id, user_name, email_address, ...updateInfo } = req.body;
-            if (!id || !user_name || !email_address) {
-                return res.json({ error: 'UserInfo with Id, user name, email address required' });
+            const { email_address, password } = req.body.request;
+            const updateInfo = req.body.request.updateInfo;
+            if (!email_address || !password) {
+                return res.json({ error: 'Password and email address are required' });
             }
-            const userInfo = { id, user_name, email_address };
-            const result = await userService.update(userInfo, updateInfo);
+
+            const user = await userService.find({ email_address });
+
+            if (!user) {
+                return res.status(404).json({ error: 'USER NOT FOUND' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(401).json({ error: 'Invalid credentials' });
+            }
+
+            const result = await userService.update({ email_address }, updateInfo);
             res.status(200).json(result);
         } catch (error) {
             next(error);
