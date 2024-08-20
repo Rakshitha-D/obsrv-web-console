45e8c653fe27cb355b91203f34803058a2b1f5d3
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-08-20 10:36:30 +0530

    #OBS-I169: modified user update api

--- src/main/controllers/user_update.ts
+++ src/main/controllers/user_update.ts
@@ -6,14 +6,12 @@ export default {
     handler: () => async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { id, user_name, email_address, ...updateInfo } = req.body;
-            const userInfo = { id, user_name, email_address };
-
-            if (!userInfo) {
+            if (!id || !user_name || !email_address) {
                 return res.json({ error: 'UserInfo with Id, user name, email address required' });
             }
-
-            const result = userService.update(userInfo, updateInfo);
-            res.json(result);
+            const userInfo = { id, user_name, email_address };
+            const result = await userService.update(userInfo, updateInfo);
+            res.status(200).json(result);
         } catch (error) {
             next(error);
         }
