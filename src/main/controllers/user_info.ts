d0e78c0e6e33a323e37ef2296ed253c5d6578e82
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-08-23 15:56:12 +0530

    #OBS-I169: updated the user info api

--- src/main/controllers/user_info.ts
+++ src/main/controllers/user_info.ts
@@ -1,18 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import _ from 'lodash';
 import userService from '../services/oauthUsers';
+import { transform } from '../../shared/utils/transformResponse';
 
 export default {
     name: 'user:info',
     handler: () => async (request: Request, response: Response, next: NextFunction) => {
         try {
-            const user = _.get(request, ['body', 'user']);
-            const data = {
-                user_name: user?.user_name,
-                email_address: user?.email_address,
-            };
-            const result = await userService.find(data);
-            response.json(result);
+            const user = _.get(request, ['body', 'request']);
+            const result = await userService.find(user);
+            if (!result) {
+                response.status(404).json(
+                    transform({
+                        params: {
+                            err: 'NOT_FOUND',
+                            errmsg: 'User not found',
+                        },
+                    }),
+                );
+            }
+            response.status(200).json(transform({ id: request.body.id, result: { id: result.id, email_address: result.email_address } }));
         } catch (error) {
             next(error);
         }
