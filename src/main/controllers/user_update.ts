e82ed68bb2d1310e5be86ed36a6e91c8d9c17b7f
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-08-26 10:24:41 +0530

    #OBS-I169: modified user update api

--- src/main/controllers/user_update.ts
+++ src/main/controllers/user_update.ts
@@ -2,30 +2,34 @@ import { Request, Response, NextFunction } from 'express';
 import userService from '../services/oauthUsers';
 import bcrypt from 'bcryptjs';
 import { transform } from '../../shared/utils/transformResponse';
+import _ from 'lodash';
 
 export default {
     name: 'user:update',
     handler: () => async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { id, email_address, password, ...rest } = req.body.request;
-            const updateInfo = rest;
-            if (!id || !email_address || !password) {
-                return res.json({ error: 'Password and email address are required' });
+            const { id, user_name, email_address, provider, ...updateInfo } = req.body.request;
+            if (!id || !user_name) {
+                return res.status(400).json({ error: 'ID and User name are required' });
+            }
+            if (_.isEmpty(updateInfo)) {
+                return res.status(400).json({ error: 'Atleast one field is required to update' });
             }
 
-            const user = await userService.find({ id, email_address });
+            if (email_address || provider) {
+                return res.status(400).json({ error: "Can't update User Name, email address or provider" });
+            }
+            const user = await userService.find({ id, user_name });
 
             if (!user) {
                 return res.status(404).json({ error: 'USER NOT FOUND' });
             }
 
-            const isMatch = await bcrypt.compare(password, user.password);
-
-            if (!isMatch) {
-                return res.status(401).json({ error: 'Invalid credentials' });
+            if (updateInfo.password) {
+                updateInfo.password = await bcrypt.hash(updateInfo.password, 12);
             }
             const result = await userService.update(
-                { id, email_address },
+                { id, user_name },
                 {
                     ...updateInfo,
                     last_updated_on: new Date().toISOString(),
